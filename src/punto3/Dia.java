94b4afaea3561ac55889bc7655508c90765b117f
jsduenass <jsduenass@unal.edu.co>
2017-04-25 20:28:45 -0500

    punto 3 vresion 0

---
+++ src/punto3/Dia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punto3;
+
+/**
+ *
+ * @author juan sebastian
+ */
+public class Dia {
+    int dia [];
+    String name;
+
+    public Dia(int[] dia, String name) {
+        this.dia = dia;
+        this.name = name;
+    }
+    
+}
---
+++ src/punto3/Segmento.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punto3;
+
+/**
+ *
+ * @author juan sebastian
+ */
+public class Segmento {
+    private int inicio [];
+    private int fin [] ;
+
+    public Segmento(int[] inicio, int[] fin) {
+        this.inicio = inicio;
+        this.fin = fin;
+    }
+    
+    
+    
+}
